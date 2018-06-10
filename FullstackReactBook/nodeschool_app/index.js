414741ec62b4073f28c92b95f684ae3879e9fdae
Lê Viết Toàn <toan.le2@tiki.vn>
2018-06-11 00:37:52 +0700

    Update code

---
+++ FullstackReactBook/nodeschool_app/index.js
@@ -0,0 +1,8 @@
+import { render } from 'react-dom';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+const element = null;
+
+render(element, container);
